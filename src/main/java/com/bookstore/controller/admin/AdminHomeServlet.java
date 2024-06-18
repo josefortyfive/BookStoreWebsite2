9243fecc704d55ad5930387116b09a3f021818a1
josefortyfive <jose.fortyfive@gmail.com>
2024-06-18 11:10:15 +0800

    Implemented login filter

--- src/main/java/com/bookstore/controller/admin/AdminHomeServlet.java
+++ src/main/java/com/bookstore/controller/admin/AdminHomeServlet.java
@@ -9,24 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin")
+@WebServlet("/admin/")
 public class AdminHomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public AdminHomeServlet() {
-        super();
-    }
 
-    
- 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+	public AdminHomeServlet() {
+		super();
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String page = "index.jsp";
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(page);
 		requestDispatcher.forward(request, response);
