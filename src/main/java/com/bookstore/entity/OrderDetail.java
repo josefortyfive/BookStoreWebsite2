d4f91fb2a859d2f216615d1211221a3a989b276e
josefortyfive <jose.fortyfive@gmail.com>
2024-09-26 11:13:33 +0800

    Added create order details

--- src/main/java/com/bookstore/entity/OrderDetail.java
+++ src/main/java/com/bookstore/entity/OrderDetail.java
@@ -8,33 +8,43 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 
+
 @Entity
 @Table(name = "order_detail", catalog = "bookstoredb")
+@NamedQueries({
+	@NamedQuery(name = "OrderDetail.bestSelling", 
+			query = "SELECT od.book FROM OrderDetail od GROUP by od.book.bookId "
+					+ "ORDER BY SUM(od.quantity) DESC"),
+	@NamedQuery(name = "OrderDetail.countByBook",
+				query = "SELECT COUNT(*) FROM OrderDetail od WHERE od.book.bookId =:bookId")
+	
+})
 public class OrderDetail implements java.io.Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private OrderDetailId id;
+	private OrderDetailId id = new OrderDetailId();
 	private Book book;
 	private BookOrder bookOrder;
+	private int quantity;
+	private float subtotal;	
 
 	public OrderDetail() {
 	}
-
 
 	public OrderDetail(OrderDetailId id) {
 		this.id = id;
 	}
 
-	public OrderDetail(OrderDetailId id, Book book, BookOrder bookOrder) {
+	public OrderDetail(OrderDetailId id, Book book, BookOrder bookOrder, int quantity, float subtotal) {
 		this.id = id;
 		this.book = book;
 		this.bookOrder = bookOrder;
+		this.quantity = quantity;
+		this.subtotal = subtotal;
 	}
 
 	@EmbeddedId
@@ -49,7 +59,6 @@ public class OrderDetail implements java.io.Serializable {
 		this.id = id;
 	}
 
-
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "book_id", insertable = false, updatable = false, nullable = false)
 	public Book getBook() {
@@ -58,6 +67,7 @@ public class OrderDetail implements java.io.Serializable {
 
 	public void setBook(Book book) {
 		this.book = book;
+		this.id.setBook(book);
 	}
 
 	@ManyToOne(fetch = FetchType.LAZY)
@@ -68,6 +78,26 @@ public class OrderDetail implements java.io.Serializable {
 
 	public void setBookOrder(BookOrder bookOrder) {
 		this.bookOrder = bookOrder;
+		this.id.setBookOrder(bookOrder);
+	}
+	
+	@Column(name = "quantity", nullable = false)
+	public int getQuantity() {
+		return this.quantity;
 	}
 
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Column(name = "subtotal", nullable = false, precision = 12, scale = 0)
+	public float getSubtotal() {
+		return this.subtotal;
+	}
+
+	public void setSubtotal(float subtotal) {
+		this.subtotal = subtotal;
+	}	
+
 }
+
